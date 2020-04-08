99890d39f6b950662227f432fa057c8cbe30282b
hhk8691608 <15625140883@163.com>
2020-04-08 16:36:33 +0800

    登录，跨域问题完成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import { Message } from 'element-ui'
+import VueLazyLoad from 'vue-lazyload'
+
 // import env from './env'
 
 Vue.config.productionTip = false
@@ -12,16 +14,16 @@ const mock = false;
 if(mock){
 	require('./mock/api');
 }
-// axios.defaults.baseURL = env.baseURL;
-axios.defaults.baseURL = '/api';
+// axios.defaults.baseURL = 'http://mall-pre.springboot.cn/api';
+axios.defaults.baseURL = 'http://127.0.0.1:8080';
 axios.defaults.timeout = 8000; 
 
 // 接口错误拦截
 axios.interceptors.response.use(function(response){
   let res = response.data;
-  if(res.status == 0){
-    return res.data;
-  }else if(res.status == 10){
+  if(res.Code == 200){
+    return res.Data;
+  }else if(res.status == 210){
     window.location.href = '/#/login';
     return Promise.reject(res);
   }else{
@@ -35,6 +37,10 @@ axios.interceptors.response.use(function(response){
 });
 
 Vue.use(VueAxios,axios);
+Vue.use(VueLazyLoad,{
+  loading:'/imgs/loading-svg/loading-bars.svg'
+})
+
 Vue.prototype.$message = Message;
 new Vue({
   router,
--- src/vue.config.js
+++ src/vue.config.js
@@ -1,15 +1,24 @@
 module.exports = {
-	devServer:{
-		host:'localhost',
-		port:8080,
-		proxy:{
-			'/api':{
-			        target:'http://mall-pre.springboot.cn',
-			        changeOrigin:true,
-			        pathRewrite:{
-			          '/api':''
-			   }
-		}
-	}
-
+  devServer:{
+    host:'localhost',
+    port:8080,
+    proxy:{
+      '/api':{
+        target:'http://mall-pre.springboot.cn',
+        ws: true,
+        changeOrigin:true,
+        pathRewrite:{
+          '/api':''
+        }
+      }
+    }
+  },
+  // publicPath:'/app',
+  // outputDir:'dist',
+  // indexPath:'index2.html',
+  // lintOnSave:false,
+  productionSourceMap:true,
+  chainWebpack:(config)=>{
+    config.plugins.delete('prefetch');
+  }
 }
